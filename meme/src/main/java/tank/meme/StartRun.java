048bb46cdb80d5d916281e51e5ac653d82ec874a
tankaixiong <tankaixiong@aliyun.com>
2014-11-18 11:47:31 +0800

    add code.java first

---
+++ meme/src/main/java/tank/meme/StartRun.java
@@ -0,0 +1,14 @@
+package tank.meme;
+
+/**
+ * 程序入口类
+ * 
+ * @author tank
+ *
+ */
+public class StartRun {
+	public static void main(String[] args) {
+		System.out.println("程序开始启动......");
+	}
+
+}
